243260da2dc986d2c9b6e10d64f119f0df87250d
noriko-terada <terada@virtual-tech.net>
2023-04-19 15:38:25 +0900

    ページネーションのOR検索カッコ対応

--- pages/api/vtecx/paging.ts
+++ pages/api/vtecx/paging.ts
@@ -11,10 +11,12 @@ const handler = async (req:NextApiRequest, res:NextApiResponse) => {
     return
   }
   // パラメータを取得
+  // req.queryだとOR検索の綴じカッコ")"パラメータが消えてしまうので、urlの値を直接切り取ってリクエストする。
   let key = ''
   let param = ''
   let pagerange = ''
   let num = undefined
+  /*
   for (const tmpkey in req.query) {
     if (tmpkey === 'key') {
       key = testutil.toString(req.query[tmpkey])
@@ -26,6 +28,102 @@ const handler = async (req:NextApiRequest, res:NextApiResponse) => {
       param = `${param}${param ? '&' : '?'}${encodeURIComponent(tmpkey)}${req.query[tmpkey] ? '=' + encodeURIComponent(testutil.toString(req.query[tmpkey])) : ''}`
     }
   }
+  */
+
+  let idx1
+  let idx1End
+  let idx2
+  let idx2End
+
+  const requestUrl:string = req.url ?? ''
+  let idxKey = requestUrl.indexOf('?key=')
+  let idxKeyEnd
+  if (idxKey == -1) {
+    idxKey = requestUrl.indexOf('&key=')
+  }
+  if (idxKey > -1) {
+    key = testutil.toString(req.query['key'])
+    idxKeyEnd = requestUrl.indexOf('&', idxKey + 1)
+    if (idxKeyEnd == -1) {
+      idxKeyEnd = requestUrl.length
+    }
+  }
+  let idxPagination = requestUrl.indexOf('&_pagination=')
+  let idxPaginationEnd
+  if (idxPagination == -1) {
+    idxPagination = requestUrl.indexOf('?_pagination=')
+  }
+  if (idxPagination > -1) {
+    pagerange = testutil.toString(req.query['_pagination'])
+    idxPaginationEnd = requestUrl.indexOf('&', idxPagination + 1)
+    if (idxPaginationEnd == -1) {
+      idxPaginationEnd = requestUrl.length
+    }
+    if (idxKey > -1) {
+      if (idxKey < idxPagination) {
+        idx1 = idxKey
+        idx1End = idxKeyEnd
+        idx2 = idxPagination
+        idx2End = idxPaginationEnd
+      } else {
+        idx1 = idxPagination
+        idx1End = idxPaginationEnd
+        idx2 = idxKey
+        idx2End = idxKeyEnd
+      }
+    } else {
+      idx1 = idxPagination
+      idx1End = idxPaginationEnd
+    }
+  }
+  let idxN = requestUrl.indexOf('&n=')
+  let idxNEnd
+  if (idxN == -1) {
+    idxN = requestUrl.indexOf('?n=')
+  }
+  if (idxN > -1) {
+    num = testutil.toNumber(req.query['n'])
+    idxNEnd = requestUrl.indexOf('&', idxN + 1)
+    if (idxNEnd == -1) {
+      idxNEnd = requestUrl.length
+    }
+
+    if (idxPagination == -1) {
+      if (idxKey > -1) {
+        if (idxKey < idxN) {
+          idx1 = idxKey
+          idx1End = idxKeyEnd
+          idx2 = idxN
+          idx2End = idxNEnd
+        } else {
+          idx1 = idxN
+          idx1End = idxNEnd
+          idx2 = idxKey
+          idx2End = idxKeyEnd
+        }
+      } else {
+        idx1 = idxN
+        idx1End = idxNEnd
+      }
+    }
+  }
+  if (idxPagination == -1 && idxN == -1 && idxKey > -1) {
+    idx1 = idxKey
+    idx1End = idxKeyEnd
+  }
+
+  if (idx1 && idx1End) {
+    param = requestUrl.substring(requestUrl.indexOf('?') + 1, idx1)
+    if (idx2 && idx2End) {
+      param += requestUrl.substring(idx1End, idx2)
+      param += requestUrl.substring(idx2End)
+    } else {
+      param += requestUrl.substring(idx1End)
+    }
+  } else {
+    param = requestUrl.substring(requestUrl.indexOf('?') + 1)
+  }
+
   const requesturi = `${key}${param}`
   const targetservice:string = testutil.getParam(req, 'targetservice')
   console.log(`[paging] key=${key} param=${param} pagerange=${pagerange} num=${String(num)} ${targetservice ? 'targetservice=' + targetservice : ''}`)
@@ -68,3 +166,4 @@ const handler = async (req:NextApiRequest, res:NextApiResponse) => {
 }
 
 export default handler
+
