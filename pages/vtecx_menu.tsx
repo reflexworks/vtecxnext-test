c8e49275506d52e75f199d6979db306a67a40024
noriko-terada <terada@virtual-tech.net>
2023-06-08 14:14:40 +0900

    メニュー画面を元に戻す

--- pages/vtecx_menu.tsx
+++ pages/vtecx_menu.tsx
@@ -1,168 +1,76 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import * as vtecxnext from '@vtecx/vtecxnext'
-import { VtecxNextError } from '@vtecx/vtecxnext'
-import * as testutil from 'utils/testutil'
-import { ApiRouteTestError } from 'utils/testutil'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import * as browserutil from 'utils/browserutil'
 
-const handler = async (req:NextApiRequest, res:NextApiResponse) => {
-  console.log(`[paging] start. x-requested-with=${req.headers['x-requested-with']}`)
-  // X-Requested-With ヘッダチェック
-  if (!vtecxnext.checkXRequestedWith(req, res)) {
-    return
-  }
-  // パラメータを取得
-  // req.queryだとOR検索の綴じカッコ")"パラメータが消えてしまうので、urlの値を直接切り取ってリクエストする。
-  let key = ''
-  let param = ''
-  let pagerange = undefined
-  let num = undefined
-  /*
-  for (const tmpkey in req.query) {
-    if (tmpkey === 'key') {
-      key = testutil.toString(req.query[tmpkey])
-    } else if (tmpkey === '_pagination') {
-      pagerange = testutil.toString(req.query[tmpkey])
-    } else if (tmpkey === 'n') {
-      num = testutil.toNumber(req.query[tmpkey])
-    } else {
-      param = `${param}${param ? '&' : '?'}${encodeURIComponent(tmpkey)}${req.query[tmpkey] ? '=' + encodeURIComponent(testutil.toString(req.query[tmpkey])) : ''}`
-    }
-  }
-  */
-
-  let idx1
-  let idx1End
-  let idx2
-  let idx2End
-
-  const requestUrl:string = req.url ?? ''
-  let idxKey = requestUrl.indexOf('?key=')
-  let idxKeyEnd
-  if (idxKey == -1) {
-    idxKey = requestUrl.indexOf('&key=')
-  }
-  if (idxKey > -1) {
-    key = testutil.toString(req.query['key'])
-    idxKeyEnd = requestUrl.indexOf('&', idxKey + 1)
-    if (idxKeyEnd == -1) {
-      idxKeyEnd = requestUrl.length
-    }
-  }
-  let idxPagination = requestUrl.indexOf('&_pagination=')
-  let idxPaginationEnd
-  if (idxPagination == -1) {
-    idxPagination = requestUrl.indexOf('?_pagination=')
-  }
-  if (idxPagination > -1) {
-    pagerange = testutil.toString(req.query['_pagination'])
-    idxPaginationEnd = requestUrl.indexOf('&', idxPagination + 1)
-    if (idxPaginationEnd == -1) {
-      idxPaginationEnd = requestUrl.length
-    }
-    if (idxKey > -1) {
-      if (idxKey < idxPagination) {
-        idx1 = idxKey
-        idx1End = idxKeyEnd
-        idx2 = idxPagination
-        idx2End = idxPaginationEnd
-      } else {
-        idx1 = idxPagination
-        idx1End = idxPaginationEnd
-        idx2 = idxKey
-        idx2End = idxKeyEnd
-      }
-    } else {
-      idx1 = idxPagination
-      idx1End = idxPaginationEnd
-    }
-  }
-  let idxN = requestUrl.indexOf('&n=')
-  let idxNEnd
-  if (idxN == -1) {
-    idxN = requestUrl.indexOf('?n=')
-  }
-  if (idxN > -1) {
-    num = testutil.toNumber(req.query['n'])
-    idxNEnd = requestUrl.indexOf('&', idxN + 1)
-    if (idxNEnd == -1) {
-      idxNEnd = requestUrl.length
-    }
-
-    if (idxPagination == -1) {
-      if (idxKey > -1) {
-        if (idxKey < idxN) {
-          idx1 = idxKey
-          idx1End = idxKeyEnd
-          idx2 = idxN
-          idx2End = idxNEnd
-        } else {
-          idx1 = idxN
-          idx1End = idxNEnd
-          idx2 = idxKey
-          idx2End = idxKeyEnd
-        }
-      } else {
-        idx1 = idxN
-        idx1End = idxNEnd
-      }
-    }
-  }
-  if (idxPagination == -1 && idxN == -1 && idxKey > -1) {
-    idx1 = idxKey
-    idx1End = idxKeyEnd
-  }
-
-  if (idx1 && idx1End) {
-    param = requestUrl.substring(requestUrl.indexOf('?') + 1, idx1)
-    if (idx2 && idx2End) {
-      param += requestUrl.substring(idx1End, idx2)
-      param += requestUrl.substring(idx2End)
-    } else {
-      param += requestUrl.substring(idx1End)
-    }
-  } else {
-    param = requestUrl.substring(requestUrl.indexOf('?') + 1)
-  }
-
-  const requesturi = `${key}${param}`
-  const targetservice:string = testutil.getParam(req, 'targetservice')
-  console.log(`[paging] key=${key} param=${param} pagerange=${pagerange} num=${String(num)} ${targetservice ? 'targetservice=' + targetservice : ''}`)
-
-  // ページング
-  let resStatus:number = 200
-  let resJson:any
-  try {
-    if (pagerange !== null && pagerange !== undefined && pagerange !== '') {
-      // ページネーション
-      resJson = await vtecxnext.pagination(req, res, requesturi, pagerange, targetservice)
-      resStatus = resJson ? 200 : 204
-    } else if (num !== null && num !== undefined) {
-      // ページ数取得
-      resJson = await vtecxnext.getPage(req, res, requesturi, testutil.toNumber(num), targetservice)
-      resStatus = resJson ? 200 : 204
-    } else {
-      console.log(`[paging] invalid type. key=${key}`)
-      throw new ApiRouteTestError(400, `[paging] invalid type. key=${key}`)
-    }
-
-  } catch (error) {
-    let resErrMsg:string
-    if (error instanceof VtecxNextError || error instanceof ApiRouteTestError) {
-      console.log(`[paging] Error occured. status=${error.status} ${error.message}`)
-      resStatus = error.status
-      resErrMsg = error.message
-    } else {
-      console.log(`[paging] Error occured. (not VtecxNextError) ${error}`)
-      resStatus = 503
-      resErrMsg = 'Error occured.'
-    }
-    resJson = {feed : {'title' : resErrMsg}}
-  }
-
-  //console.log(`[paging] end. resJson=${JSON.stringify(resJson)}`)
-  res.status(resStatus)
-  resJson ? res.json(resJson) : 
-  res.end()
+const Header = ({title} : {title:string}) => {
+  return <h1>{title ? title : 'Default title'}</h1>
 }
 
-export default handler
+const HomePage = () => {
+
+  const router = useRouter()
+  const [isLoggedIn, setLoggedIn] = useState('')
+
+  useEffect(() => {
+    if (router.isReady) {
+      getLoggedIn()
+    }
+  }, [router])
+
+  const getLoggedIn = async () => {
+    console.log('[getLoggedIn] start.')
+
+    const method = 'GET'
+    const apiAction = 'isloggedin'
+    const data = await browserutil.requestApi(method, apiAction, '')
+    let result = 'false'
+    if (data) {
+      if ('feed' in data && 'title' in data.feed) {
+        result = data.feed.title
+        console.log(`[getLoggedIn] isLoggedIn=${result}`)
+      }
+    } else {
+      console.log(`[getLoggedIn] data is null.`)
+    }
+    setLoggedIn(result)
+  }
+
+  return (
+    <div>
+      <Header title="vtecxnext テスト メニュー (develop)" />
+      <label>【useEffect】 is logged in: {isLoggedIn}</label>
+      <br/>
+      <br/>
+      <Link href='/vtecx_login'>vtecxnext ログイン</Link> (vtecx_login)
+      <br/>
+      <Link href='/vtecx_test'>vtecxnext テスト</Link> (vtecx_test)
+      <br/>
+      <Link href='/vtecx_signup'>ユーザ登録</Link> (vtecx_signup)
+      <br/>
+      <Link href='/vtecx_passresetp'>パスワード変更</Link> (vtecx_passreset)
+      <br/>
+      <Link href='/vtecx_staticprops'>StaticProps テスト</Link> (vtecx_staticprops)
+      <br/>
+      <Link href='/vtecx_viewpdf'>PDF表示テスト</Link> (vtecx_viewpdf)
+
+      <br/>
+      <br/>
+      <Link href='/stripe/payment'>カード決済 テスト （決済とカード登録を同時に行う）</Link> (stripe/payment)
+      <br/>
+      <Link href='/stripe/checkout'>カード決済 テスト （カード登録を行った後、決済を行う）</Link> (stripe/checkout)
+      <br/>
+      <Link href='/stripe/subscription'>サブスクリプション テスト （決済とカード登録を同時に行う）</Link> (stripe/subscription)
+      <br/>
+      <Link href='/stripe/payment'>カード決済 テスト （決済とカード登録を同時に行う）</Link> (stripe/payment)
+
+      <br/>
+      <br/>
+      <Link href='/vtecx_menu_getserversideprops'>vtecxnext メニュー 【getServerSideProps】</Link> (vtecx_menu_getserversideprops)
+      <br/>
+
+    </div>
+  )
+}
+
+export default HomePage
