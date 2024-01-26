55632ed3bf24d74d6144d0b422d1288727c571e0
noriko-terada <terada@virtual-tech.net>
2024-01-26 16:44:00 +0900

    healthcheck追加

---
+++ app/api/healthcheck/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest } from 'next/server'
+import { VtecxNext, VtecxNextError } from 'utils/vtecxnext'
+
+export const GET = async (req:NextRequest):Promise<Response> => {
+  console.log(`[api healthcheck] start.`)
+
+  const vtecxnext = new VtecxNext(req)
+
+  return vtecxnext.response(200)
+}
