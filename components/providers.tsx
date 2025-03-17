61e01b838ae52cfaedc5d3b6f3eb4ce87aba85a8
noriko-terada <terada@virtual-tech.net>
2025-03-17 17:16:34 +0900

    deploy

--- components/providers.tsx
+++ components/providers.tsx
@@ -5,7 +5,7 @@ import { ReCaptchaProvider } from 'next-recaptcha-v3'
 export const Providers = ({children}:any) => {
   const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_KEY ?? ''
   return (
-    <ReCaptchaProvider reCaptchaKey={siteKey} useEnterprise>
+    <ReCaptchaProvider reCaptchaKey={siteKey} useEnterprise >
       {children}
     </ReCaptchaProvider>
   )
